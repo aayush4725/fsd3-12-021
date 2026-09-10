e3244ce000e5dbfae5327d1aee42055f213c9e58
aayush4725 <aayush.25b01011332@abes.ac.in>
2026-09-10 11:09:47 +0530

    API

--- lab4/prg7.js
+++ lab4/prg7.js
@@ -26,4 +26,4 @@ const server = http.createServer((req, res) => {
         res.end();
     }
 });
-server.listen(3000, () => console.log("prg6 is running..."));
+server.listen(3000, () => console.log("prg7 is running..."));
