8c69f2ef1ebcd8434c84dd3995a244b0af98024b
aayush4725 <aayush.25b01011332@abes.ac.in>
2026-09-11 16:13:09 +0530

    users

---
+++ lab4/users.js
@@ -0,0 +1,8 @@
+// we use in memory database
+let user = [
+    {id: 1, name :'Amir sharma' , mob : '9192xxxxxx' , email : 'aamir343@gmail.com'},
+        {id:1, name :'Amir sharma' , mob : '9192xxxxxx' , email : 'aamir343@gmail.com'},
+
+]
+ let nextId = 3;
+export const getUsers = () => users;
